43e639c45daa0974f9a36391b9ce4efdedc509ea
garvpatidar04 <patidargarv04@gmail.com>
2024-12-20 17:38:39 +0530

    updating database.py, config.py and alembic/env.py to deply on render

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
+    # database_hostname: str
+    # database_port: str
+    # database_password: str
+    # database_name: str
+    # database_username: str
+    database_url: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes : int
